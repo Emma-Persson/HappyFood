c39452488257803efe2e25b5627d72848f205ec1
Emma Persson <empe0009@stud.ek.dk>
2026-03-12 17:15:47 +0100

    knapper og variabler på opskrifter

--- js/opskrifter.js
+++ js/opskrifter.js
@@ -15,8 +15,9 @@ fetch(`https://dummyjson.com/recipes?&limit=100`).then((res) =>
 function showProducts(dataArr) {
   recipesCatalog.innerHTML = `
   <h2>Alle opskrifter</h2>
+  <h3>0 - 30 min</h3>
   <section>
-    <h3>filtrer:</h3>
+    <h5>filtrer:</h5>
     <div>
       <button class="european">European</button>
       <button class="asian">East & South-East Asian</button>
@@ -25,26 +26,22 @@ function showProducts(dataArr) {
       <button class="american">North and Latin American</button>
     </div>
   </section>
-  <section id="recipesList"></section>
+  <section id="recipesList" class="grid_1-1-1"></section>
   `;
   const recipesList = document.querySelector("#recipesList");
   dataArr.forEach((recipes) => {
-    recipesList.innerHTML += `
-<section class="grid_1-1-1">
-        <div>
+    recipesList.innerHTML += `<div>
           <img src="https://cdn.dummyjson.com/recipe-images/${recipes.id}.webp" alt="" />
           <h4>${recipes.name}</h4>
           <p>Tid: ${recipes.cookTimeMinutes + recipes.prepTimeMinutes} minutter</p>
-          <div><button>${recipes.difficulty}</button><button>${recipes.cuisine}</button></div>
-          <button>Læs mere</button>
-        </div>
-      </section>
-   `;
+          <div><button class="difficulty ${recipes.difficulty.toLowerCase()}">${recipes.difficulty}</button><button class="cuisine ${recipes.cuisine.toLowerCase().replace(" ", "-")}">${recipes.cuisine}</button></div>
+          <button class="read-more">Read More</button>
+        </div>`;
     console.log(`${recipes.cookTimeMinutes + recipes.prepTimeMinutes}`);
   });
 
   document.querySelector(".asian").addEventListener("click", () => filterCuisine(["Asian", "Japanese", "Korean", "Thai", "Vietnamese"]));
-  document.querySelector(".european").addEventListener("click", () => filterCuisine(["Italian", "Spanish", "Russian"]));
+  document.querySelector(".european").addEventListener("click", () => filterCuisine(["Italian", "Spanish", "Russian", "French"]));
   document.querySelector(".mediterranean").addEventListener("click", () => filterCuisine(["Greek", "Turkish", "Lebanese", "Moroccan", "Mediterranean"]));
   document.querySelector(".southasian").addEventListener("click", () => filterCuisine(["Indian", "Pakistani"]));
   document.querySelector(".american").addEventListener("click", () => filterCuisine(["American", "Mexican", "Brazilian", "Cuban", "Hawaiian"]));
